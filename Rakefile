5055a54789f0dcdde2ff5ae0025530f512419b88
Daniel Westendorf <daniel@prowestech.com>
2010-12-21 07:35:55 -0800

    Version bump to 0.0.0

--- Rakefile
+++ Rakefile
@@ -15,14 +15,11 @@ Jeweler::Tasks.new do |gem|
   gem.name = "sinatra-subroutes"
   gem.homepage = "http://github.com/danielwestendorf/sinatra-subroutes"
   gem.license = "MIT"
-  gem.summary = %Q{TODO: one-line summary of your gem}
-  gem.description = %Q{TODO: longer description of your gem}
+  gem.summary = %Q{Allows restricting routes to a subdomain}
+  gem.description = %Q{Restrict routes in Sinatra based of the Subdomain in the URI}
   gem.email = "daniel@prowestech.com"
   gem.authors = ["Daniel Westendorf"]
-  # Include your dependencies below. Runtime dependencies are required when using your gem,
-  # and development dependencies are only needed for development (ie running rake tasks, tests, etc)
-  #  gem.add_runtime_dependency 'jabber4r', '> 0.1'
-  #  gem.add_development_dependency 'rspec', '> 1.2.3'
+  gem.add_runtime_dependency 'sinatra', ">=0"
 end
 Jeweler::RubygemsDotOrgTasks.new
 
---
+++ lib/sinatra-subroutes.rb
@@ -0,0 +1,41 @@
+# This method is heavily adapted from the Rails method of determining the subdomain.
+require 'rubygems'
+require 'sinatra'
+
+require 'rack/request'
+
+# Extracted from http://tannerburson.com/2009/01/extracting-subdomains-in-sinatra.html
+# We re-open the request class to add the subdomains method
+module Rack
+  class Request
+    def subdomains(tld_len=1) # we set tld_len to 1, use 2 for co.uk or similar
+      # cache the result so we only compute it once.
+      @env['rack.env.subdomains'] ||= lambda {
+        # check if the current host is an IP address, if so return an empty array
+        return [] if (host.nil? ||
+                      /\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$/.match(host))
+        host.split('.')[0...(1 - tld_len - 2)] # pull everything except the TLD
+      }.call
+    end
+  end
+end
+
+module Sinatra
+  class Base
+    class << self
+
+      # Adapted from here: http://toms-toolbox.com/2010/04/sinatra-native-subdomain-routing/
+      def subdomain(pattern)
+        condition {
+          if request.subdomains[0] =~ pattern
+            @params[:subdomain] = $~[1..-1]
+            true
+          else
+            false
+          end
+        }
+      end
+
+    end
+  end
+end
